8a608a42e9b037480fd7dff8db267e912902ddc6
Willby Hudson <willhudson04@gmail.com>
2024-10-28 12:54:31 +1100

    Show and hide pf

--- scripts/interface.js
+++ scripts/interface.js
@@ -266,6 +266,7 @@ ChoreoGraph.graphicTypes.titleScreen = new class titleScreen {
     g.playHover = false;
     g.creditsToggleHover = false;
     g.showCredits = false;
+    g.foundation = false;
   }
   draw(g,cg) {
     cg.c.fillStyle = "#fafafa";
@@ -281,7 +282,9 @@ ChoreoGraph.graphicTypes.titleScreen = new class titleScreen {
       cg.c.imageSmoothingEnabled = false;
       cg.drawImage(cg.images.ChoreoGraph,750,250+5,400*0.3,400*0.3,20,false);
       cg.drawImage(cg.images.aseprite,570,250,400*0.35,400*0.35,0,false);
-      cg.drawImage(cg.images.penguinfoundation,575,70,311*0.8,160*0.8,0,false);
+      if (g.foundation) {
+        cg.drawImage(cg.images.penguinfoundation,575,70,311*0.8,160*0.8,0,false);
+      }
       cg.c.font = "50px Lilita";
       cg.c.textAlign = "center";
       cg.c.fillText("A game made by Willby",580,-200);
